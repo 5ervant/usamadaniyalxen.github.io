459427197f5c21942ec46aef37fdb49be66b0b2f
Usama Daniyal Xen <usamadaniyalxen@gmail.com>
2015-07-16 00:32:28 +0500

    Updated

---
+++ envato/themeforest/liner/js/sound-mouseover.js
@@ -0,0 +1,1 @@
+function playclip(){if(navigator.appName=="Microsoft Internet Explorer"){if(document.all){document.all.sound.src="../audio/click.wav";}}else{{var audio=document.getElementsByTagName("audio")[0];audio.play();}}}
