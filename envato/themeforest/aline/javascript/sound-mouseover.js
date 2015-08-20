b2953d1017ac3310574fbc401382460663c4414e
Usama Daniyal Xen <usamadaniyalxen@gmail.com>
2015-08-21 02:11:16 +0500

    Updated

--- envato/themeforest/aline/javascript/sound-mouseover.js
+++ envato/themeforest/aline/javascript/sound-mouseover.js
@@ -1,25 +1,5 @@
-/* ==============================================================
-
-   Author: UDX
-   Author URL: http://themeforest.net/user/UDX
-   Template: Aline Dental Treatment - HTML5 Template
-   Template URL: http://themeforest.net/item/x/12394553
-   Version: 1.0
-
-   ============================================================== */
-
-function playclip() {
-if (navigator.appName == "Microsoft Internet Explorer") {
-if (document.all)
- {
-  document.all.sound.src = "#"; // If you wanted to change hover effect sound and you want to play hover effect
- }                              // sound on all browser you will have to convert your sound in three format
-}                               // mp3, wav and oog because every broswer not run mp3 or other formats
-                                // so you can add your audio link here if you have convert your mp3
-else {                          // to wav beacause of internet explorer.
-{
-var audio = document.getElementsByTagName("audio")[0];
-audio.play();
+function playclip(){if(navigator.appName=="Microsoft Internet Explorer"){if(document.all)
+{document.all.sound.src="#";}
 }
-}
-}
+
+else{{var audio=document.getElementsByTagName("audio")[0];audio.play();}}}
